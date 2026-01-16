5aac7680034eac0e09bfcdca8cc3ba9dbaf7e6fc
Bhawesh Choudhary <bhaweshchoudhary100@gmail.com>
2026-01-16 23:00:08 +0530

    Implementing Struct

--- rust-bootcamp2/src/main.rs
+++ rust-bootcamp2/src/main.rs
@@ -102,19 +102,19 @@
 
 
 struct Rect {
-    widht: u32,
+    width: u32,
     height: u32,
 }
 
 impl Rect {
     fn area(&self) -> u32 {
-        self.widht * self.height
+        self.width * self.height
     }
 }
-
+    
 fn main() {
     let rect = Rect {
-        widht: 30,
+        width: 30,
         height: 50,
     };
     println!("The area of the rectangle is {}", rect.area());
